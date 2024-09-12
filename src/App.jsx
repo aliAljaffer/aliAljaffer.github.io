276ec2398722c49f5d39c0940b2541ca0165e28e
Ali Aljaffer <89468030+aliAljaffer@users.noreply.github.com>
2024-09-12 15:56:18 +0300

    Lazy loading and suspense implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { lazy, Suspense } from "react";
 import "./index.css";
 import About from "./components/About";
 import Box from "./components/Box";
 import Directions from "./components/Directions";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PageNotFound from "./components/PageNotFound";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Images from "./components/Images";
+import Spinner from "./components/Spinner";
+// import PageNotFound from "./components/PageNotFound";
+// import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
+// import Images from "./components/Images";
+const Images = lazy(() => import("./components/Images"));
+const Projects = lazy(() => import("./components/Projects"));
+const Contact = lazy(() => import("./components/Contact"));
+const PageNotFound = lazy(() => import("./components/PageNotFound"));
 
 function App() {
   const width = " w-[85dvw] sm:w-[85dvw] lg:w-[80dvw] lg:max-w-[1024px]";
@@ -27,9 +33,30 @@ function App() {
         >
           <Routes>
             <Route index path="/" element={<About w={width} h={height} />} />
-            <Route path="/photography" element={<Images />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/projects" element={<Projects />} />
+            <Route
+              path="/photography"
+              element={
+                <Suspense fallback={<Spinner />}>
+                  <Images />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/contact"
+              element={
+                <Suspense fallback={<Spinner />}>
+                  <Contact />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/projects"
+              element={
+                <Suspense fallback={<Spinner />}>
+                  <Projects />
+                </Suspense>
+              }
+            />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
         </Box>
--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,6 +1,6 @@
 function PageNotFound() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 p-4 text-4xl">
+    <div className="mb-auto mt-auto flex flex-col items-center gap-4 p-4 text-4xl">
       <h1 className="text-center">404</h1>
       <p className="text-center">Page not found!</p>
     </div>
